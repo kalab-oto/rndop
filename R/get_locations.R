175a4453b45ac86d5a64bfce049b6dbf774a7bf9
kalab-oto <kalab.oto@gmail.com>
2025-02-05 01:19:39 +0100

    fix: location link

--- R/get_locations.R
+++ R/get_locations.R
@@ -9,14 +9,16 @@ get_locations <- function(filter_session_info) {
     if (httr::content(locat,"text") == "\n\n\n\n\n\n") {
        cat("No locations available")
     } else {
-        locat_ind <- gregexpr("http.[^>]*zip", httr::content(locat,"text"))
+        locat_ind <- gregexpr("export[^>]*zip", httr::content(locat,"text"))
         cat(length(locat_ind[[1]]))
         sf_list <- list()
         for (i in 1:length(locat_ind[[1]])) {
-            locat_url <- substring(httr::content(locat, "text"),
+            locat_url <- paste0("https://portal23.nature.cz/nd/",
+                                substring(httr::content(locat, "text"),
                                    locat_ind[[1]][i],
                                    locat_ind[[1]][i] + attr(locat_ind[[1]],
                                                           "match.length")[i]-1)
+                                )
             cat(locat_url)
             locat_temp_zip <- tempfile()
             download.file(url = locat_url, destfile = locat_temp_zip)
