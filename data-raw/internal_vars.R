69830e93d9c1130c69f187d36867fa02d18e4954
kalab-oto <kalab.oto@gmail.com>
2025-02-06 12:39:26 +0100

    fix: generate testing polygons

--- data-raw/internal_vars.R
+++ data-raw/internal_vars.R
@@ -1,4 +1,22 @@
 USER_AGENT <- paste0("rndop/",packageVersion("rndop")," (https://github.com/kalab-oto/rndop)")
-                  
+
+# polygons for spatial query
+
+poly_1_df <- data.frame(x = c(-529744, -528744),
+                        y = c(-1115254, -1116254))
+
+poly_1 <- sf::st_as_sfc(
+    sf::st_bbox(
+        sf::st_as_sf(poly_1_df, coords = c("x", "y"), crs = 5514))
+        )
+
+poly_120_df <- data.frame(x = c(-529744 - 5000, -528744 + 5000),
+                        y = c(-1115254 + 5000, -1116254 - 5000))
+
+poly_120 <- sf::st_as_sfc(
+    sf::st_bbox(
+        sf::st_as_sf(poly_120_df, coords = c("x", "y"), crs = 5514))
+        )
+
 sysdata_filenames <- load("R/sysdata.rda")
 save(list = c(sysdata_filenames, "USER_AGENT"), file = "R/sysdata.rda")
