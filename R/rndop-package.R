dcdcab904289293c219972f91b353faf77346e5a
kalab-oto <kalab.oto@gmail.com>
2025-02-06 10:57:22 +0100

    docs: change title, typo

--- R/rndop-package.R
+++ R/rndop-package.R
@@ -1,4 +1,4 @@
-#' @title Interface for The Species Occurrence Database of NCA CR (Czechia)
+#' @title Access to the Data from The Species Occurrence Database of NCA CR (Czechia) in R
 #' 
 #' @description A programmatic interface to the The Species Occurrence Database 
 #' of NCA CR (NDOP AOPK). The NDOP is Czech national species occurrence database 
@@ -6,7 +6,7 @@
 #' <https://portal.nature.cz/nd/>). Database includes occurrence records both 
 #' from professionals (monitorings, inventarisations) and citizen scientists. 
 #' Use of the database is free and requires free registration 
-#' (<https://idm.nature.cz/idm/#/egistration>). Data itselfs are published 
+#' (<https://idm.nature.cz/idm/#/egistration>). Data itself are published 
 #' under CC BY-SA 4.0 licence and should be referenced according to AOPK 
 #' citation rules 
 #' (<https://portal23.nature.cz/publik_syst/ctihtmlpage.php?what=4910>).
